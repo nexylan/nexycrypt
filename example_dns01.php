76ad89dc20bdf02de2f9437170388cccc2e94b16
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-01-27 12:19:53 +0100

    DNS challenge

---
+++ example_dns01.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Usage:
+ *
+ * $ composer install
+ * php example.php example.com 1
+ * # Copy generated file under public folder on .well-known/acme-challenge folder from you domain webroot.
+ * php example.php example.com 2
+ * # Get the certificate files on cert folder
+ */
+
+use GuzzleHttp\Exception\ClientException;
+use Nexy\NexyCrypt\Authorization\Challenge\Http01Challenge;
+use Nexy\NexyCrypt\NexyCrypt;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+if ($argc < 3) {
+    echo 'You have to pass domain and step arguments.'.PHP_EOL;
+    exit(1);
+}
+
+$domains = [];
+for ($a = 1; $a < $argc - 1; ++$a) {
+    $domains[] = $argv[$a];
+}
+$step = intval($argv[$a]);
+
+// First commented line is for production.
+//$client = new NexyCrypt();
+$client = new NexyCrypt(null, 'https://acme-staging.api.letsencrypt.org/');
+
+try {
+    $client->register();
+    $client->agreeTerms();
+
+    if (1 === $step) {
+        @mkdir('public');
+
+        foreach ($domains as $domain) {
+            $authorization = $client->authorize($domain);
+
+            $challenge = $authorization->getChallenges()->getDns01();
+
+            echo sprintf('Record Name: %s.%s', $challenge->getRecordName(), $domain).PHP_EOL;
+            echo sprintf('Record Type: %s', $challenge->getRecordType()).PHP_EOL;
+            echo sprintf('Record Content: %s', $challenge->getRecordContent()).PHP_EOL;
+
+            @mkdir('public/'.$domain);
+            file_put_contents('public/'.$domain.'/challenge', serialize($challenge));
+        }
+    }
+
+    if (2 === $step) {
+        foreach ($domains as $domain) {
+            /** @var Http01Challenge $challenge */
+            $challenge = unserialize(file_get_contents('public/'.$domain.'/challenge'));
+
+            if (false === $client->verifyChallenge($challenge)) {
+                echo sprintf('Invalid challenge for %s', $domain).PHP_EOL;
+            };
+        }
+
+        @mkdir('cert');
+
+        $certificate = $client->generateCertificate($domains);
+        foreach ($certificate->getFilesArray() as $filename => $content) {
+            file_put_contents('cert/'.$filename, $content);
+        }
+
+        $certificate = $client->signCertificate($certificate);
+        foreach ($certificate->getFilesArray() as $filename => $content) {
+            file_put_contents('cert/'.$filename, $content);
+        }
+    }
+} catch (ClientException $e) {
+    dump(json_decode($e->getResponse()->getBody()->getContents(), true));
+
+    exit(1);
+}
--- src/Authorization/Challenge/Dns01Challenge.php
+++ src/Authorization/Challenge/Dns01Challenge.php
@@ -2,6 +2,8 @@
 
 namespace Nexy\NexyCrypt\Authorization\Challenge;
 
+use Base64Url\Base64Url;
+
 /**
  * @author Sullivan Senechal <soullivaneuh@gmail.com>
  */
@@ -13,5 +15,29 @@ final class Dns01Challenge extends AbstractChallenge
     public function getType()
     {
         return ChallengeInterface::DNS_01;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecordName()
+    {
+        return '_acme-challenge';
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecordType()
+    {
+        return 'TXT';
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecordContent()
+    {
+        return Base64Url::encode(hash('sha256', $this->authorizationKey, true));
     }
 }
