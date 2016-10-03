bbae44c3fa116871bdf3e03e646de9412b24a22b
peter279k <peter279k@gmail.com>
2016-10-03 08:43:23 +0800

    complete unit testing ver2

--- tests/NexyCryptTest.php
+++ tests/NexyCryptTest.php
@@ -135,6 +135,17 @@ class NexyCryptTest extends TestCase
         }
     }
 
+    /** @test */
+    public function getKeyTest()
+    {
+        @unlink(sys_get_temp_dir().$this->keyPath);
+
+        $cryptClient = new NexyCrypt(null, $this->url);
+        $cryptClient->createKey();
+
+        $this->assertSame(true, file_exists(sys_get_temp_dir().$this->keyPath));
+    }
+
     public function verifyChallengeTest(NexyCrypt $cryptClient)
     {
         $challenge = unserialize(file_get_contents(__DIR__.'/'.'public'.'/acme-challenge'.'/challenge'));
